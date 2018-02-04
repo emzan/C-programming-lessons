cd03300eac6a008a62e4422b82e637e320690465
emzan <zanettiemilio@hotmail.com>
2018-02-04 22:29:56 +0800

    Adding values and variables

---
+++ C_02_13.14.15/02_13_value1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Here is a value: %d\n", 27);
+  printf("Here is a value: %d\n", 29);
+
+  return (0);
+
+}
---
+++ C_02_13.14.15/02_13_value2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Everyone knows that 2+2=%d\n", 4);
+
+
+  return (0);
+
+}
