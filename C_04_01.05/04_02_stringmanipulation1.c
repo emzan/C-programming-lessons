049249449f9df46fc43c49e3f621a836eea6993c
minCai <zanettiemilio@hotmail.com>
2018-02-05 19:14:02 +0800

    Update 04_02_stringmanipulation1.c

--- C_04_01.05/04_02_stringmanipulation1.c
+++ C_04_01.05/04_02_stringmanipulation1.c
@@ -4,4 +4,12 @@
 int main()
 {
   char string[] = "Just how long am I?";
+  int len;
+  
+  len = strlen(string);
+  printf("The following string:\n");
+  puts(string);
+  printf("is %d characters long.\n",len);
+  
+  return(0);
 }
