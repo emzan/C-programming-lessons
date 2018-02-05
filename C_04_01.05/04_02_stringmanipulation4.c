77c5050a37dbc32abf2a192ccc4b2a62775bb8da
minCai <zanettiemilio@hotmail.com>
2018-02-05 19:25:51 +0800

    Update 04_02_stringmanipulation4.c

--- C_04_01.05/04_02_stringmanipulation4.c
+++ C_04_01.05/04_02_stringmanipulation4.c
@@ -2,3 +2,17 @@
 #include <string.h>
 
 int main()
+{
+  char password[9];
+  int compare;
+  
+  printf("Enter your password: ");
+  scanf("%8s",password);
+  compare = strcmp(password,"secret");
+  if( compare = 0)
+    puts("You've been granted access!");
+  else
+    puts("Intruder alert!");
+  
+  return(0);
+}
