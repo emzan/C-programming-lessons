b9421f2f084b1312a16d5a9041cd12d44d238bab
emzan <zanettiemilio@hotmail.com>
2018-02-05 12:26:30 +0800

    No equal

--- C_03_01.03/03_01_if2.c
+++ C_03_01.03/03_01_if2.c
@@ -12,8 +12,8 @@ int main ()
     printf("%d is greater than 10.\n", a);
 
   }
-  if (a<=10) {
-    printf("%d is less than or equal to 10.\n", a);
+  if (a<10) {
+    printf("%d is less than 10.\n", a);
   }
 
   return(0);
