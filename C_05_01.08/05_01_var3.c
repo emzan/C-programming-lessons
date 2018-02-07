62ee5637afb5bd0799b4fd46d2867605053bdda1
minCai <zanettiemilio@hotmail.com>
2018-02-07 23:59:14 +0800

    Update 05_01_var3.c

--- C_05_01.08/05_01_var3.c
+++ C_05_01.08/05_01_var3.c
@@ -2,6 +2,14 @@
 
 int main()
 {
-
-
+  int a;
+  char b;
+  float c;
+  
+  puts("Memory locations:");
+  printf("A is at %p\n",&a);
+  printf("B is at %p\n",&b);
+  printf("C is at %p\n",&c);
+  
+  return(0);
 }
