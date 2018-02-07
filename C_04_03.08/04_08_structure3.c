a320b2a7e4d4690194b85de5a06300c4a6c40e00
minCai <zanettiemilio@hotmail.com>
2018-02-07 16:29:57 +0800

    Update 04_08_structure3.c

--- C_04_03.08/04_08_structure3.c
+++ C_04_03.08/04_08_structure3.c
@@ -2,5 +2,17 @@
 
 int main()
 {
+  struct person{
+    char name[32];
+    int age;
+  };
+  struct person president;
+  
+  president.name = "George Washington";
+  president.age = 67;
+  
+  printf("%s was %d years old\n",president.name,president.age);
+  
+  return(0)';
 
 }
