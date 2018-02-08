1fad0ac86b67a11d8fa71f597994f8c3e3647818
emzan <zanettiemilio@hotmail.com>
2018-02-08 14:51:30 +0800

    int version

--- C_05_01-08/05_02_pointer2.c
+++ C_05_01-08/05_02_pointer2.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 
+int main()
+{
+  int pokey;
+  int *p;
 
-
-int main() {
-
-  char pokey;
-  char *p;
-
-  pokey = 'Q';
+  pokey = 987;
   p = &pokey;
 
-  printf("The address of pokey is %p\n", &pokey);
-  printf("The contents of pokey are %c\n", pokey);
+  printf("The address of 'pokey' is %p\n", &pokey);
+  printf("The contents of 'pokey' are %d\n", pokey);
 
-
-  printf("The address of pokey is %p\n", p);
-  printf("The contents of pokey are %c\n", *p);
+  printf("The address of 'pokey' is %p\n", p);
+  printf("The contents of 'pokey' are %d\n", *p);
 
   return(0);
-
-
 }
