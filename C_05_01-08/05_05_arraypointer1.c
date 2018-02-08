675a811c50b9807bd9904fa9c22226f6b1e7af1c
emzan <zanettiemilio@hotmail.com>
2018-02-08 15:01:38 +0800

    starting pointer file

--- C_05_01-08/05_05_arraypointer1.c
+++ C_05_01-08/05_05_arraypointer1.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
 
-
-
 int main() {
 
   int array[] = { 11, 13, 17, 19 };
   int x;
-  int *aptr;
-
-  aptr = array;
-  //aptr = aptr +2;
-  //*aptr = 0;   //these two lines are abbreviated below
-  *(aptr +2) = 0;
 
   for (x = 0; x < 4; x++) {
     printf("Element %d: %d\n", x+1, array[x] );
