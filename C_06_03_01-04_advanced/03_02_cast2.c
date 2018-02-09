cf99e1885765bbcb290c0deb15a901cbcfd95509
emzan <zanettiemilio@hotmail.com>
2018-02-09 21:34:50 +0800

    cast to four

---
+++ C_06_03_01-04_advanced/03_02_cast2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  int a;
+  float aroot;
+
+  printf("Type an integer: ");
+scanf("%d",&a);
+aroot = sqrt(a);
+  printf("The square foot of %d is %f\n",a,aroot);
+
+  return (0);
+}
