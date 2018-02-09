d44d431aa7ca094852a8409d0babdc50ec5b5730
emzan <zanettiemilio@hotmail.com>
2018-02-09 16:58:19 +0800

    intro a0;

--- C_01_01_intro/01_01_assignments.c
+++ C_01_01_intro/01_01_assignments.c
@@ -6,26 +6,6 @@ int main()
 
   printf("v equals %d\n",v);
 
-
-  v = v + 20;
-  printf("v + 20 equals %d\n", v);
-
-
-  v = v - 2;
-  printf("v - 2 equals %d\n", v);
-
-
-  v = v / 3;
-  printf("v / 3 equals %d\n", v);
-
-
-  v = v * 4;
-  printf("v * 4 equals %d\n", v);
-
-
-  v = v % 7;
-  printf("v %% 7 equals %d\n", v);
-
   return (0);
 
 }
