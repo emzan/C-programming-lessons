f47ee170ecc31ae4f965abf116857f8909741095
emzan <zanettiemilio@hotmail.com>
2018-02-09 13:34:58 +0800

    first

--- C_01_01-04/01_01_assignments.c
+++ C_01_01-04/01_01_assignments.c
@@ -2,8 +2,30 @@
 
 int main()
 {
-  puts("I am  no longer a dummy");
-  
-  return 3;
+  int v = 0;
+
+  printf("v equals %d\n",v);
+
+
+  v = v + 20;
+  printf("v + 20 equals %d\n", v);
+
+
+  v = v - 2;
+  printf("v - 2 equals %d\n", v);
+
+
+  v = v / 3;
+  printf("v / 3 equals %d\n", v);
+
+
+  v = v * 4;
+  printf("v * 4 equals %d\n", v);
+
+
+  v = v % 7;
+  printf("v %% 7 equals %d\n", v);
+
+  return (0);
 
 }
