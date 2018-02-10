f904ca4a5ca724ade307d1a81c9eb7d62764e4ac
emzan <zanettiemilio@hotmail.com>
2018-02-10 21:04:09 +0800

    arrAdvanced

---
+++ C_06_04_10_13_advanced/04_10_structarray4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+  struct weather {
+    char day[10];
+    float temp;
+  };
+  struct weather today = {
+    {"Sunday", 72.5 },
+    {"Monday", 72.5 },
+    {"Tuesday", 72.5 },
+    {"Wednesday", 72.5 },
+    {"Thursday", 72.5 },
+    {"Friday", 72.5 },
+    {"Saturday", 72.5 }
+  };
+  int x;
+
+  week[1] = week[5];
+
+  puts("This wee's forecast:");
+	for(x=0;x<7;x++)
+		printf("%10s %.1f degrees\n",
+	week[x].day,
+	week[x].temp);
+
+	return(0);
+}
---
+++ C_06_04_10_13_advanced/04_12_solution.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main()
+{
+  struct weather {
+    char day[10];
+    float temp;
+  };
+  struct weather week[7] = {
+    {"Sunday", 72.5 },
+    {"Monday", 72.5 },
+    {"Tuesday", 72.5 },
+    {"Wednesday", 72.5 },
+    {"Thursday", 72.5 },
+    {"Friday", 72.5 },
+    {"Saturday", 72.5 }
+  };
+  struct weather t;
+  int x,outer,inner;
+
+
+  puts("Unsorted Temperature Array:");
+  for (x = 0; x < 7; x++)
+  printf("%10s %.1f degrees\n",
+week[x].day,
+week[x].temp);
+
+
+for (outer = 0; outer < 7; outer++)
+{
+  for (inner = outer+1; inner < 7; inner++)
+  {
+    if (week[outer].temp > week[inner].temp )
+    {
+      t = week[inner];
+      week[inner] = week[outer];
+      week[outer] = t;
+    }
+  }
+}
+
+
+puts("Sorted Temperature Array:");
+for(x=0;x<7;x++)
+  printf("%10s %.1f degrees\n",
+week[x].day,
+week[x].temp);
+
+	return(0);
+}
