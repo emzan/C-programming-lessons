279a31ac78c9f328d46e7a5214a84ce5869b371d
emzan <zanettiemilio@hotmail.com>
2018-02-12 14:49:28 +0800

    two pluses

--- C_05_advanced_01-03/05_02_pointer2.c
+++ C_05_advanced_01-03/05_02_pointer2.c
@@ -4,13 +4,13 @@ int main()
 {
   char string[] = "Greetings from the digital realm!\n";
   char *s;
-  
+
   s = string;
   while(*s)
   {
-    putchar( ==*s );
+    putchar( ++*s );
     s++;
   }
-  
+
   return(0);
 }
