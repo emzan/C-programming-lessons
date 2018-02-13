c94a5c3030df23a9ebad2b4b02c6c6109a9587db
minCai <zanettiemilio@hotmail.com>
2018-02-13 17:23:45 +0800

    Update 05_04_solution.c

--- C_05_01-08/05_04_solution.c
+++ C_05_01-08/05_04_solution.c
@@ -12,7 +12,7 @@ int main() {
   
 
   printf("The address of pokey is %p\n", &pokey);
-printf("The address of pokey is %c\n", pokey);
+  printf("The address of pokey is %c\n", pokey);
 
   printf("The address of pokey is %p\n", p);  
   printf("The address of pokey is %c\n", *p);
