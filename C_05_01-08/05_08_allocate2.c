c831c37db3048f9d475e06189d70c6cc01f03991
emzan <zanettiemilio@hotmail.com>
2018-02-14 10:51:57 +0800

    back in black

--- C_05_01-08/05_08_allocate2.c
+++ C_05_01-08/05_08_allocate2.c
@@ -3,12 +3,12 @@
 
 #define SIZE 1024
 
-int main() 
+int main()
 {
     char *sto;
 
     sto = malloc(sizeof(char)*SIZE);
-    if (sto == NULL) 
+    if (sto == NULL)
     {
       puts("Memory error");
       return(1);
@@ -16,9 +16,6 @@ int main()
     else
     {
       printf("%d bytes of memory allocated at %p\n",SIZE,sto);
-      printf("Type something: ");
-      fgets(sto,SIZE,stdin);
-      printf("You typed:\n%s\n",sto);
     }
 
     return(0);
