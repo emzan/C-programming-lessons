80c835a746153a5d6b35ef993ab3543e2f3b401c
emzan <zanettiemilio@hotmail.com>
2018-02-15 12:21:13 +0800

    commiits comment

--- C_06_04_10_13_advanced/04_10_structarray1.c
+++ C_06_04_10_13_advanced/04_10_structarray1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
+//commmits
 int main()
 {
 	struct weather{
@@ -9,7 +9,7 @@ int main()
 	};
 	struct weather week[7];
 	int x;
-	
+
 	strcpy(week[0].day,"Sunday");
 	week[0].temp = 72.5;
 	strcpy(week[1].day,"Monday");
@@ -24,12 +24,12 @@ int main()
 	week[5].temp = 92.5;
 	strcpy(week[6].day,"Saturday");
 	week[6].temp = 92.8;
-	
+
 	puts("This wee's forecast:");
 	for(x=0;x<7;x++)
 		printf("%10s %.1f degrees\n",
 	week[x].day,
 	week[x].temp);
-	
+
 	return(0);
 }
