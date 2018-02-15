d8e20850316dbd426b5b46886b71d6fb93907ecb
emzan <zanettiemilio@hotmail.com>
2018-02-15 12:44:45 +0800

    comments to the file

--- C_05_11-12/05_12_linkedlist1.c
+++ C_05_11-12/05_12_linkedlist1.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main() {
 
   struct stats{
     int account;
     float balance;
-
   };
   struct stats *first;
   int a = 1;
 
+/* create new structure */
   first = (struct stats *)malloc(sizeof(struct stats));
   if( first == NULL)
   {
@@ -19,17 +18,15 @@ int main() {
     return(1);
   }
 
+/* fill the structure */
   first->account = a;
   printf("Account %05d, enter the balance: $",first->account);
   scanf("%f",&first->balance);
 
+/* display the structure */
   printf("Account %05d\tBalance: %.2f\n",
 first->account,
 first->balance);
 
-
-
 return(0);
-
-
-    }
+}
