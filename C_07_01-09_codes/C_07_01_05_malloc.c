07077da13088b993fc96f760f8c7727a64747fd2
emzan <zanettiemilio@hotmail.com>
2018-02-18 12:58:05 +0800

    mallochioz

---
+++ C_07_01-09_codes/C_07_01_05_malloc.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int a_size = 11;
+  int *a = NULL;
+
+  a = malloc(a_size * sizeof(int));
+  if(a == NULL)
+  {
+    fprintf(stderr,"Memory allocating error!\n");
+    exit(1);
+  }
+
+  for(int i=0; i<a_size; i++){
+    a[i] = i*12;
+  }
+
+  for (int i = 0; i < a_size; i++) {
+    printf("a[%d]: %d\n",i,a[i]);
+  }
+
+  free(a);
+
+   return (0);
+}
