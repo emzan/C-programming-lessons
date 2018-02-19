20dfc7e9e79b3dee18f8c06ce7d68441506ca7d5
emzan <zanettiemilio@hotmail.com>
2018-02-19 19:09:59 +0800

    lastCommitArrayRealloc

--- C_07_01-09_codes/C_07_01_07_realloc1.c
+++ C_07_01-09_codes/C_07_01_07_realloc1.c
@@ -9,16 +9,25 @@ void print_int_array(int *b, int b_size)
   }
 }
 
+int* create_int_array(int lenght)
+{
+  int *a = malloc(lenght*sizeof(int));
+  if(a==NULL)
+  {
+    return a;
+  }
+
+  for (int i = 0; i < lrnght; i++) {
+    a[i] = i*1000;
+  }
+  return a;
+
+}
+
 int main()
 {
   int a_size = 10;
-
-  int *a = malloc(a_size * sizeof(int));
-
-  for(int i=0;i<a_size;i++)
-  {
-    a[i] = i*100;
-  }
+  int *a = create_int_array(a_size);
 
   print_int_array(a,a_size);
 
