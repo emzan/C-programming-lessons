4712c3c7592ef219f5ec75163edc17da3a178e6e
emzan <zanettiemilio@hotmail.com>
2018-02-28 16:41:55 +0800

    accountInMyBank

--- C_04_03-09/04_08_structure1.c
+++ C_04_03-09/04_08_structure1.c
@@ -7,6 +7,10 @@ int main()
     float balance;
   };
   struct record my_bank;
-  
+
+  my_bank.account = 123456;
+  my_bank.balance = 6543.21;
+  printf("In account %d I have %.2f\n",my_bank.account,my_bank.balance);
+
   return(0);
 }
